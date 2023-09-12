6899fb661328765b558f57fb457218d37f40d03b
CharlyBarie <cbarie@gmail.com>
2023-09-11 23:25:25 -0300

    fix

--- js/scripts/login.js
+++ js/scripts/login.js
@@ -1,11 +1,11 @@
-let ActiveUser = false; 
+let ActiveUser = false; // Utiliza camelCase para los nombres de variables
 document.getElementById("guardarLogin").addEventListener("click", guardado);
 function guardado() {
     let user = document.getElementById("user").value;
     let pass = document.getElementById("password").value;
-    ActiveUser = login(user, pass); 
+    ActiveUser = login(user, pass); //verifico en el array si esta el usurio
     if (ActiveUser) {
-        showMenu()
+        BotoneraPrincipal();
         console.log("Credenciales guardadas con éxito.");
     } else {
         console.log("Error de ingreso");
@@ -15,13 +15,19 @@ function guardado() {
 function login(a, b) {
     for (const item of Usuarios) {
         if (item.user == a && item.password == b) {
-            const UsuarioEnLocal = JSON.stringify(item);
+            const UsuarioEnLocal = JSON.stringify(item);//almaceno todo el item para trabajar
             localStorage.setItem("item", UsuarioEnLocal);
-            let objetoDesdeLocalStorage = JSON.parse(localStorage.getItem("item"));
-            return true;
+
+            
+            return true 
         };
     };
-    return false; 
+    return false; // Devuelve false si no se encuentra un usuario coincidente
 };
-
-
+ // Recupera el objeto del localStorage
+ let objetoDesdeLocalStorage = JSON.parse(localStorage.getItem("item"));
+ cl(objetoDesdeLocalStorage)
+function cl(a){
+    console.log(a);
+    console.log(typeof(a));
+} 
